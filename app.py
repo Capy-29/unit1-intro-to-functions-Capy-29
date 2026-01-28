275b3f4a0cb49d2593c094be170785a173c6abf9
Capy-29 <temirc@nycstudents.net>
2026-01-28 09:22:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+
+t.speed(10)
+
+def circle(d):
+    length = d*3.14/360
+    for i in range(360):
+        t.forward(length)
+        t.right(1)
+
+for i in range(13):
+    circle(200)
+    t.right(360/13)
+
+
+turtle.done()
