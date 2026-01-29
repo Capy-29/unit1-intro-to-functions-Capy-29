fd8dcb2101b0078e6da93123a92ed752dd274535
Capy-29 <temirc@nycstudents.net>
2026-01-29 09:26:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,30 @@ t = Turtle()
 
 t.speed(10)
 
+
+def square(size):
+    for i in range(4):
+        t.forward(size)
+        t.right(90)
+
+
 def circle(d):
     length = d*3.14/360
     for i in range(360):
         t.forward(length)
         t.right(1)
 
-for i in range(13):
-    circle(200)
-    t.right(360/13)
+def mondala(size, circles):
+    for i in range(circles):
+        circle(size)
+        t.right(360/circles)
 
+def spiral(squares):
+    for i in range(squares):
+        square(375/squares * i)
+        t.right(300/squares)
+
+
+spiral(50)
 
 turtle.done()
